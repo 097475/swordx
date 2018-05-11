3b6069b04376f4b4065faca995fd89c6569c753f
Alessandro Giacche` <jack@jim-laptop.local>
2018-05-11 16:00:32 +0200

    Syncronized

--- swordx.c
+++ swordx.c
@@ -4,255 +4,115 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <ctype.h>
-#include "swordx.h"
+#include "trie.h"
 
+static int cmpstringp ( const void* , const void* );
+int isInArray ( char** , long , char* );
 
-void printall(Trie *t)
-{
-	if(t!=NULL)
-	{
-		printf("value: %s\toccurrences: %d\n",t->value,t->occurrencies);
-		for(int i = 0; i<26; i++)
-		{
-			printall(t->children[i]);		
-		}
-	}
-	
-
-	
-}
-
-void addRecord(char* str, Trie *t, int level){
-Trie *n = (Trie*)malloc(sizeof(Trie));
-
-        if(t->children[str[level-1]-'a'] == NULL)
-        {           
-            n->value = (char*)malloc(level+1);
-            strncpy(n->value,str,level);  //bad function
-           // snprintf(n->value,level+1,"%s",str); //space for null character
-           // n->value=str;
-           	 n->value[level] = '\0';
-            //printf("%s %d\n",n->value,level);
-            
-            
-            for(int j = 0; j<26; j++)
-			{
-				n->children[j] = NULL;
-			}
-			n->occurrencies=0;
-			t->children[str[level-1]-'a'] = n;
-        }
-            if(level==strlen(str))
-            {
-                n->occurrencies++;
-                free(str);
-            }
-            else{
-                level++;
-                addRecord(str,n,level);
-            }
-}
-
-void add (char* str, Trie *t, int level){ //start with 1
-int flag = 0;
-Trie *next;
-
-	next = t->children[str[level-1]-'a'];
-    if(next != NULL && strncmp(str,next->value,level)==0)
-    {
-        //matched
-        if(level == strlen(str))
-        {
-            // add count
-            next->occurrencies++;
-        }
-        else{
-            level++;
-            add(str,next,level);
-        }
-
-        flag = 1; //insert successful
-    }
-
-
-
-if(flag==0)
-{
-	addRecord(str,t,level);
-}
-//unmatched, create record(s)
-//printf("%s%d",str,level);
-
-
-}
-
-FILE* open_file(char* path){
+FILE* open_file(char* path) {
 	FILE* pf = fopen(path,"rb");
-
-	if(pf==NULL)
-	{
-		perror("");
+	if(pf == NULL) {
+		perror("Error reading");
 		exit(EXIT_FAILURE);
 	}
 	return pf;	
 }
 
-char* getWord(FILE *pf)
-{
-	char c;
-	char buf[500];
-	int pos = 0;
-	while((c=getc(pf))==' ');
-	ungetc(c,pf);
-	while((c=getc(pf))!=' ' && c!='\n' && c!=EOF)
-	{
-		//printf("%c ",c);
-		if(isalpha(c))
-		{
-			//printf("%c\n",c);
-			if(isupper(c))
-			{
-				c = tolower(c);
-			}
-			buf[pos] = c;
-			pos++;
-		}
-		if(pos==500)
-		{
+char* getWord(FILE *pf, char *blacklist[], int blc) {
+	char c, buf[500]; // c is the character, buf is the word
+	int pos = 0; // index
+	
+	while(!isalpha(c = getc(pf)) && c != EOF); // remove all not-alphabetic character before a string
+	ungetc(c,pf); // put the last char back
+
+	while(isalpha((c = getc(pf)))) {
+		buf[ pos++ ] = tolower(c);
+		if(pos == 500) {
 			fprintf(stderr,"Word is too long\n");
 			exit(EXIT_FAILURE);
 		}
-
 	}
-	//printf("%d ",pos);
-	if(c==EOF)
-	{
-		return NULL;
-	}
-	//printf("buf: %s\n",buf);
-	char *word = (char*)malloc((pos+1)*sizeof(char));
-	//int tes = snprintf(word,pos+1,"%s",buf); //space for null character
-	//word[pos]='\0';
-	strncpy(word,buf,pos); //bad function
-	word[pos] = '\0';
-	//printf("%s %s pos:%d\n",buf,word,pos);
+	if(c == EOF) return NULL;
+	char *word = (char*)malloc(pos*sizeof(char));
+	strncpy(word,buf,pos);
+	word[pos]='\0';
 	
-	// clear word
-	
-	return word; 
+	if(!isInArray(blacklist,blc,word))
+		return word;
+	free(word);
+	return getWord(pf,blacklist,blc);
 }
 
-FILE* makeFile()
-{
+
+FILE* makeFile() {
 	FILE *pf = fopen("result.txt","wb");
-	if(pf == NULL)
-	{
-		perror("");
+	if(pf == NULL) {
+		perror("Error writing");
 		exit(EXIT_FAILURE);
 	}
-	
 	return pf;
 }
 
-void writeWord(Trie *t,FILE *pf)
-{
+void writeWord(Trie *t,FILE *pf) {
 	char *word = t->value;
-	int occurrencies = t->occurrencies;
-	
 	fwrite(word,sizeof(char),strlen(word),pf);
-	putc(':',pf);
-	putc(' ',pf);
-	putc(occurrencies+'0',pf);
-	putc('\r',pf);
-	putc('\n',pf);
+	fputs(": ",pf);
+	putc(t->occurrencies+'0',pf);
+	fputs("\r\n",pf);
 }
 
-
-void visitTree(Trie *t, FILE *pf){
-	
-	if(t!=NULL){
-		
-		if(t->occurrencies>0)
-		{
-			writeWord(t,pf);
-		}
-		
-		for(int i = 0; i<26; i++)
-		{
-			visitTree(t->children[i],pf);		
-		}
-		
-	}
-	
+void visitTree(Trie *t, FILE *pf) {
+	if(t == NULL) return;
+	if(t->occurrencies > 0) // if the word occurrs at least one time...
+		writeWord(t,pf); // ... write the word in the file
+	for(int i = 0; i < 26; i++)
+		visitTree(t->children[i],pf); // check each children of the node
 }
 
-Trie* createTree()
-{
-	Trie *t = (Trie*)malloc(sizeof(Trie));
-	t->value="";
-	t->occurrencies=0;
-	for(int i = 0; i<26; i++)
-	{
-		t->children[i] = NULL;
-	}
-	return t;
-}
-
-void execute(char *src)
-{
+void execute(char *src, char *blacklist[], int blc) {
 	FILE *pfread = open_file(src);
 	Trie *t = createTree();
 	char *str;
-	while((str=getWord(pfread))!=NULL)
-	{
-		add(str,t,1);
-	}
+	if(blc > 1) qsort(blacklist, blc, sizeof(char*),cmpstringp);
+	while((str = getWord(pfread,blacklist,blc)) != NULL)
+		add(str,t); // add the word to the trie (starting from the 1st level)
 	FILE *pfwrite = makeFile();
-	visitTree(t,pfwrite);
+	visitTree(t,pfwrite); // write trie status
 	fclose(pfread);
 	fclose(pfwrite);
-	//printall(t);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+	char *arr[] = {"one","two","three","four","five","six","seven"};	
+	char *src;
 	int option_index = 0;
-	int opt;
-    static struct option long_options[] = {
-    {"add",     optional_argument, 0,  0 },
-    {"append",  no_argument, 0,  0 },
-    {"delete",  no_argument, 0,  0 },
-    {"verbose", no_argument, 0,  0 },
-    {"create",  no_argument, 0,  0 },
-    {"file",    no_argument, 0,  0 },
+	int opt; // optaaantains the getopt_long return value (the index of long_options array
+    static struct option long_options[] = { // "--comand" , argument required? , X , opt result
+		{ "exclude",     required_argument, 0,  0 }
     };
-		
- while ((opt = getopt_long(argc, argv, "f:",long_options,&option_index)) != -1)
- {
- 	switch(opt)
- 	{
- 		case 0:  printf("option %s", long_options[option_index].name); break;
- 		case 'f':
-            printf("from file(s)  %s\n",optarg);
-            execute(optarg);
-            break;
-	}
- }
 
-/*char* str = "test";
-add(str,t,1);
-add("tent",t,1);
-add("test",t,1);
-add("sternocleidomastoideo",t,1);
-add("concupiscenza",t,1);*/
+	while ((opt = getopt_long(argc, argv, "f:",long_options,&option_index)) != -1) // read all the args, for each arg...
+		switch(opt) { // ... switch it
+			case 0:
+				//printf("Option %s\n\tArgs: %s\n", long_options[option_index].name,optarg);
+				printf("Blacklist\n\t%s\n", optarg);
+				//arr = stringToArray(optarg);
+				break;
+			case 'f':
+				printf("Reading from file(s)\t%s\n",optarg);
+				src = optarg;
+				break;
+		}
+	
+	execute(src,arr,sizeof(arr)/sizeof(*arr)); // optarg contains the filename
+	//execute(optarg,NULL,0);
+}
 
+int isInArray(char *array[], long length, char *str) {
+	char **key = &str;
+	return bsearch(key, array, length, sizeof(array[0]), cmpstringp) != NULL;
+}
 
-
-
-//printall(t);
-
-
-/*printf("%s",getWord(pf));
-printf("%s",getWord(pf));
-printf("%s",getWord(pf));*/
+static int cmpstringp(const void *p1, const void *p2) { // from "man qsort": the qsort method accepts void pointers only
+	return (strcmp(*(char **)p1, *(char **)p2));
 }
